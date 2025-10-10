d1b6fc1c17e307ae71d4bfeed785e39db3846c77
sauronevillord <sauronevillord@gmail.com>
2025-10-10 16:47:57 +0200

    collegato

--- scripts/engine.js
+++ scripts/engine.js
@@ -3,14 +3,20 @@ import * as THREE from "three";
 const EngineType = {
     FLUIDPARTICLE: 0,
     GRIDVIRUS: 1,
-}
+};
+
+const FluidRenderMode = {
+    PARTICLES: 0,
+    FLUID: 1,
+};
+
+const FRUSTUM_SIZE = 1;
 
 export default class Engine extends EventTarget {
     constructor () {
         super();
         this.initTHREE();
-        this.fluidParticleEngine = this.fluidParticleEngine();
-        this.gridVirusEngine = new this.gridVirusEngine();
+        this.initEngines();
 
         // INIT SIMULATION AND RENDERING
         this.activeEngine = EngineType.FLUIDPARTICLE;
@@ -22,41 +28,69 @@ export default class Engine extends EventTarget {
         this.simulation = this.simulation.bind(this);
         this.resizeCanvas = this.resizeCanvas.bind(this);
 
+        // Scene
         this.scene = new THREE.Scene();
+
+        // Camera
         this.camera = new THREE.OrthographicCamera();
+        this.camera.near = 0.1;
+        this.camera.far = 1000;
+
+        // Renderer
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
         });
+        this.renderer.setClearColor(0xffffff);
+
+        // Canvas
         this.canvas = document.body.appendChild(this.renderer.domElement);
         window.addEventListener('resize', this.resizeCanvas);
+
         this.resizeCanvas();
     }
 
-    resizeCanvas() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+    initEngines () {
+        this.fluidParticleEngine = this.fluidParticleEngine();
+        this.gridVirusEngine = this.gridVirusEngine();
+        this.fluidParticleEngine.init();
+        this.gridVirusEngine.init();
     }
 
-    pauseSimulation() {
+    resizeCanvas () {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        const aspectRatio = this.canvas.width / this.canvas.height;
+        this.camera.left = FRUSTUM_SIZE * aspectRatio / -2;
+        this.camera.right = FRUSTUM_SIZE * aspectRatio / 2;
+        this.camera.top = FRUSTUM_SIZE / 2;
+        this.camera.bottom = FRUSTUM_SIZE / -2;
+
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(this.canvas.width, this.canvas.height);
+    }
+
+    pauseSimulation () {
         this.simulationStopped = true;
         this.signalSimulationToggled();
     }
 
-    startSimulation() {
+    startSimulation () {
         this.simulationStopped = false;
         this.signalSimulationToggled();
     }
 
-    toggleSimulation() {
+    toggleSimulation () {
         this.simulationStopped = !this.simulationStopped;
         this.signalSimulationToggled();
     }
 
-    reset() {
+    reset () {
         console.log("reset");
     }
 
-    getActiveEngine() {
+    getActiveEngine () {
         switch (this.activeEngine) {
             case EngineType.FLUIDPARTICLE:
                 return this.fluidParticleEngine;
@@ -65,7 +99,7 @@ export default class Engine extends EventTarget {
         }
     }
 
-    getSimulationPlayIcon() {
+    getSimulationPlayIcon () {
         return this.simulationStopped ? '▶' : '⏸';
     }
 
@@ -83,25 +117,14 @@ export default class Engine extends EventTarget {
         this.render(deltaTime);
 
         requestAnimationFrame(this.simulation);
-    }
-
-    render (deltaTime) {
-        switch (this.activeEngine) {
-            case EngineType.FLUIDPARTICLE:
-                return this.renderFluid();
-            case EngineType.GRIDVIRUS:
-                return this.renderGridVirus();
-        }
 
         this.signalFrameRendered(deltaTime);
     }
 
-    renderFluid () {
+    render () {
+        this.getActiveEngine().render();
 
-    }
-
-    renderGridVirus () {
-
+        this.renderer.render(this.scene, this.camera);
     }
 
     signalFrameRendered (deltaTime) {
@@ -138,6 +161,10 @@ export default class Engine extends EventTarget {
         const SPIKY_COEFF = -45.0 / (Math.PI * h6);
         const LAPLACIAN_COEFF = 45.0 / (Math.PI * h5);
 
+        const MAX_PARTICLES = 1000;
+        const GRAVITY = 9.8;
+        const FORCE_ATTRITION = 0.2;
+
         const Wpoly6 = function (r2) {
             let temp = h2 - r2;
             return WPOLY_COEFF * temp * temp * temp;
@@ -157,11 +184,14 @@ export default class Engine extends EventTarget {
                 // Particle positon
                 x: Math.random() * (engine.camera.right - engine.camera.left) + engine.camera.left,
                 y: Math.random() * (engine.camera.bottom - engine.camera.top) + engine.camera.top,
+
                 // Particle velocity
                 Vx: Math.random() - 0.5,
                 Vy: Math.random() - 0.5,
+
                 rho: 0,     // Density
                 p: 0,       // Pressure
+
                 // Particle applied force
                 Fx: 0,
                 Fy:0,
@@ -169,19 +199,46 @@ export default class Engine extends EventTarget {
         };
 
         return {
-            simulationStep: function (deltaTime) {
+            particles: [],
+            particleMeshes: [],
+            particleGeometry: new THREE.CircleGeometry(10, 16),
+            particleMaterial: new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+            init: function () {
+                this.particles = new Array(MAX_PARTICLES);
+                this.particlesMeshes = new Array(MAX_PARTICLES);
+                for (let i = 0; i < MAX_PARTICLES; i++) {
+                    this.particles[i] = particle();
+                    this.particleMeshes[i] = new THREE.Mesh(this.particleGeometry, this.particleMaterial);
+                    engine.scene.add(this.particleMeshes[i]);
+                }
+            },
+            applyForces: function (deltaTime) {
 
             },
+            simulationStep: function (deltaTime) {
+                this.applyForces(deltaTime);
+            },
+            render: function () {
+                for (let i = 0; i < MAX_PARTICLES; i++) {
+                    this.particleMeshes[i].position.x = this.particles[i].x;
+                    this.particleMeshes[i].position.y = this.particles[i].y;
+                }
+            }
         };
     }
 
     gridVirusEngine () {
 
         return {
+            init: function () {
 
+            },
             simulationStep: function (deltaTime) {
 
             },
+            render: function () {
+
+            }
         };
     }
 }
